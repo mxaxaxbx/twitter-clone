bdbf18a62e12d7b8c13e8d9a66cb230e40b89e51
Miguel Arenas <miguel.arenas@sitidoctor.com>
2022-01-20 14:36:07 -0500

    register users

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -14,7 +14,7 @@ import (
 func Manejadores() {
 	router := mux.NewRouter()
 
-	router.HandleFunc("/registro", middlewares.ChequoBD(routers.Registro)).Methods("POST")
+	router.HandleFunc("/registro", middlewares.ChequeoBD(routers.Registro)).Methods("POST")
 
 	PORT := os.Getenv("PORT")
 	if PORT == "" {
