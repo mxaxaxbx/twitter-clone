fc8c48164059a03501e5bd9ebb2320b44eb6c477
Miguel Arenas <miguel.arenas@sitidoctor.com>
2022-01-20 17:43:00 -0500

    the routine of check if user does exists has been added

---
+++ bd/chequeoYaExisteUsuario.go
@@ -0,0 +1,30 @@
+package bd
+
+import (
+	"context"
+	"time"
+
+	"github.com/mxaxaxbx/twitter-clone/models"
+	"go.mongodb.org/mongo-driver/bson"
+)
+
+func ChequeoExisteUsuario(email string) (models.Usuario, bool, string) {
+	ctx, cancel := context.WithTimeout(context.Background(), 15*time.Second)
+	defer cancel()
+
+	db := MongoCN.Database("twitter-clone")
+	col := db.Collection("usuarios")
+
+	condicion := bson.M{"email": email}
+
+	var resultado models.Usuario
+
+	err := col.FindOne(ctx, condicion).Decode(&resultado)
+	ID := resultado.ID.Hex()
+
+	if err != nil {
+		return resultado, false, ID
+	}
+
+	return resultado, true, ID
+}
