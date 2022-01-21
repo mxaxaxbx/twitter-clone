28ef34074dfaefbc1c29a4b3d5ee0a96c7d4cc06
Miguel Arenas <miguel.arenas@sitidoctor.com>
2022-01-21 14:46:12 -0500

    the login routine has been added

---
+++ bd/intentoLogin.go
@@ -0,0 +1,26 @@
+package bd
+
+import (
+	"github.com/mxaxaxbx/twitter-clone/models"
+	"golang.org/x/crypto/bcrypt"
+)
+
+func IntentoLogin(email string, password string) (models.Usuario, bool) {
+	usu, encontrado := ChequeoYaExisteUsuario(email)
+
+	if encontrado == false {
+		return usu, false
+	}
+
+	passwordBytes := []byte(password)
+	passwordBD := []byte(usu.Password)
+
+	err := bcrypt.CompareHashAndPassword(passwordBD, passwordBytes)
+
+	if err != nil {
+		return usu, false
+	}
+
+	return usu, true
+
+}
