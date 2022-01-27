0c2f12ab89d0adeeec8710c908d52eb7d6ec9015
Miguel Arenas <miguel.arenas@sitidoctor.com>
2022-01-27 16:11:57 -0500

    The routinw of update profile has been created

---
+++ bd/modificoRegistro.go
@@ -0,0 +1,61 @@
+package bd
+
+import (
+	"context"
+	"time"
+
+	"github.com/mxaxaxbx/twitter-clone/models"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+func ModificoRegistro(u models.Usuario, ID string) (bool, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 15*time.Second)
+	defer cancel()
+
+	db := MongoCN.Database("twitter")
+	col := db.Collection("usuarios")
+
+	registro := make(map[string]interface{})
+
+	if len(u.Nombre) > 0 {
+		registro["nombre"] = u.Nombre
+	}
+
+	if len(u.Apellidos) > 0 {
+		registro["apellidos"] = u.Apellidos
+	}
+	registro["fechaNacimiento"] = u.FechaNacimiento
+
+	if len(u.Avatar) > 0 {
+		registro["avatar"] = u.Avatar
+	}
+
+	if len(u.Banner) > 0 {
+		registro["banner"] = u.Banner
+	}
+
+	if len(u.Biografia) > 0 {
+		registro["biografia"] = u.Biografia
+	}
+
+	if len(u.SitioWeb) > 0 {
+		registro["sitioWeb"] = u.SitioWeb
+	}
+
+	updtString := bson.M{
+		"$set": registro,
+	}
+
+	objId, _ := primitive.ObjectIDFromHex(ID)
+	filtro := bson.M{"_id": bson.M{"$eq": objId}}
+
+	_, err := col.UpdateOne(ctx, filtro, updtString)
+
+	if err != nil {
+		return false, err
+	}
+
+	return true, nil
+
+}
