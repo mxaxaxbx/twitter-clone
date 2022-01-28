563692940f773f7a4ad759ad54103db441460697
Miguel Arenas <miguel.arenas@sitidoctor.com>
2022-01-28 17:32:26 -0500

    the routine of save tweet has been added

---
+++ bd/insertoTweet.go
@@ -0,0 +1,32 @@
+package bd
+
+import (
+	"context"
+	"time"
+
+	"github.com/mxaxaxbx/twitter-clone/models"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+func InsertoTweet(t models.GraboTweet) (string, bool, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 15*time.Second)
+	defer cancel()
+
+	db := MongoCN.Database("twitter")
+	col := db.Collection("tweet")
+
+	registro := bson.M{
+		"userid":  t.UserID,
+		"mensaje": t.Mensaje,
+		"fecha":   t.Fecha,
+	}
+
+	result, err := col.InsertOne(ctx, registro)
+
+	if err != nil {
+		return string(""), false, err
+	}
+
+	return result.InsertedID.(primitive.ObjectID).String(), true, nil
+}
---
+++ models/graboTweet.go
@@ -0,0 +1,11 @@
+package models
+
+import (
+	"time"
+)
+
+type GraboTweet struct {
+	UserID  string    `bson:"userid" json:"userid,omitempty"`
+	Mensaje string    `bson:"mensaje" json:"mensaje,omitempty"`
+	Fecha   time.Time `bson:"fecha" json:"fecha,omitempty"`
+}
